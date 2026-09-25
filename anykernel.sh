7e8bda78d5d43323487378e19af8048a0b007133
Muhammad Guntur <mochtur@gmail.com>
2026-09-25 09:39:52 +0700

    anykernel: Fix missing end of case in miui checking

--- anykernel.sh
+++ anykernel.sh
@@ -142,6 +142,7 @@ else
       
       mv *-miui-dtbo.img $home/dtbo.img
       rm *-aosp-dtbo.img
+    ;;
     *)
       ui_print "Default variant detected !!!";
       ui_print "Using Regular AOSP DTBO... ";
